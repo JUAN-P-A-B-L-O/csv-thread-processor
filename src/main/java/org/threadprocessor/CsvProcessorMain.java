7260e1e5aebc12cf03f250ec72a9e4252dd64e23
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2025-12-06 21:52:02 -0300

    chore: add Amazon sample CSV to resources

---
+++ src/main/java/org/threadprocessor/CsvProcessorMain.java
@@ -0,0 +1,13 @@
+package org.threadprocessor;
+
+import java.nio.file.Path;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CsvProcessorMain {
+    public static void main(String[] args) {
+//        String amazonCsvFilePath = Path.of("data/Amazon.csv");
+        System.out.printf("Hello and welcome!");
+
+    }
+}
---
+++ src/main/java/org/threadprocessor/io/CsvFileReader.java
@@ -0,0 +1,9 @@
+package org.threadprocessor.io;
+
+public class CsvFileReader {
+    private java.io.FileReader stringReader;
+
+    public CsvFileReader(String filePath){
+
+    }
+}
