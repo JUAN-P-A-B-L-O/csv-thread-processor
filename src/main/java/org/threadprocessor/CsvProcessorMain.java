2b83c9502334767423ab32e2e5ef94a3f910bc53
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2025-12-07 00:32:38 -0300

    feat: add initial amazon csv scanner to CsvProcessorMain

--- src/main/java/org/threadprocessor/CsvProcessorMain.java
+++ src/main/java/org/threadprocessor/CsvProcessorMain.java
@@ -1,13 +1,24 @@
 package org.threadprocessor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CsvProcessorMain {
-    public static void main(String[] args) {
-//        String amazonCsvFilePath = Path.of("data/Amazon.csv");
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        Path amazonCsvFilePath = Path.of("src/main/resources/data/Amazon.csv");
+
+        try(Scanner amazonCsvScanner = new Scanner(amazonCsvFilePath)){
+            for(int i = 0; i < 10 && amazonCsvScanner.hasNextLine(); i++){
+                System.out.println(amazonCsvScanner.nextLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
